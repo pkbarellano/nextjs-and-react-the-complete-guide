3330040779ce7333c60e1ff0f965a8b8ea006f17
pkbarellano@gmail.com <pkbarellano@gmail.com>
2025-02-26 16:51:18 +0800

    214. Creating a New Lucia Auth Instance

---
+++ section9-user-authentication/lib/auth.js
@@ -0,0 +1,17 @@
+const { Lucia } = require('lucia');
+import { BetterSqlite3Adapter } from '@lucia-auth/adapter-sqlite';
+import db from './db';
+
+const adapter = new BetterSqlite3Adapter(db, {
+    user: 'users',
+    session: 'sessions'
+});
+
+const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        expires: false,
+        attributes: {
+            secure: process.env.NODE_ENV === 'production'
+        }
+    }
+});
