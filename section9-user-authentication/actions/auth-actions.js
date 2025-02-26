2cc7f56a44374c7604d9479a0d7d5202e6804864
pkbarellano@gmail.com <pkbarellano@gmail.com>
2025-02-26 15:59:57 +0800

    210. Hashing Passwords & Storing User Data The Right Way

--- section9-user-authentication/actions/auth-actions.js
+++ section9-user-authentication/actions/auth-actions.js
@@ -1,5 +1,6 @@
 'use server';
 
+import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
 
 export async function signup(prevState, formData) {
@@ -22,5 +23,6 @@ export async function signup(prevState, formData) {
         };
     }
 
-    createUser(email, password);
+    const hashedPassword = hashUserPassword(password);
+    createUser(email, hashedPassword);
 };
